08c4f3d9cfa3ef1951e3f656fd2a0a70248ab0d5
Judd Chalmers Melvin <juddmelvinhtmldev@gmail.com>
2024-06-24 18:46:18 -0400

    added meetgreet,stage, and event

---
+++ migrations/20240622181018-create-stage.js
@@ -0,0 +1,25 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Stages', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        type: Sequelize.INTEGER
+      },
+      stage_id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Stages');
+  }
+};
